e8e7503ea80c5abe504ac4d76603350269ba17e0
Ronak Sankhala <RonakSankhala@outlook.com>
2024-08-21 20:30:39 +0530

    Add project files.

---
+++ TechSodeECommerceApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechSodeECommerceApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+    }
+}
